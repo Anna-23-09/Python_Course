13b07e2ed51de1d8f1e5b8d7f1a15b649cfb3893
Anna <tolstovaaniuta@yandex.ru>
2022-09-26 14:29:42 +0300

    2 task

--- 1_Homework/2_HW.py
+++ 1_Homework/2_HW.py
@@ -5,5 +5,5 @@ print("x y z")
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            if not (x != y != z):
-                print(x, y, z)
+            if (not (x or y or z)) == (not x or not y or not z):
+                print("Утверждение истинно")
