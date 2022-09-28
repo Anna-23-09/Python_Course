718342c7e52600e8d8e91cad7882ea8a37fd254c
Anna <tolstovaaniuta@yandex.ru>
2022-09-28 15:18:50 +0300

    3 task

--- 2_Homework/3_HW.py
+++ 2_Homework/3_HW.py
@@ -10,5 +10,21 @@ for i in range(1, n + 1):
     i += 1
     n = round(n + 1)
     result = result + n
+    
 print(list)
 print(result)
+
+# n = int(input("Введите количество чисел в списке: "))
+# list = []
+
+# for i in range(1, n + 1):
+#     list.append(round((1 + 1/i) ** i))
+
+# print(list)
+
+# result = 0
+# i = 0
+# while i < n:
+#     result = round(result + i, 0)
+#     i = i + 1
+# print(result)
