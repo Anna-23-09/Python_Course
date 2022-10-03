e2b82c4731afa2e8418bb9b576727a19b6d71a49
Anna <tolstovaaniuta@yandex.ru>
2022-10-03 15:40:28 +0300

    2 task

--- 3_Homework/2_HW.py
+++ 3_Homework/2_HW.py
@@ -2,12 +2,20 @@
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 from random import sample
-def prod (count, number):
-    my_list = sample(range(1, 11), count)
-    print(my_list)
-    result = 1
-    n = 1
-    if n in 
-    
-    
-print(num_in_list(int(input()), int(input())))
+
+def new_list (size):
+    list_of_numbers = sample(range(1, 11), size)
+    return list_of_numbers
+
+def prod_of_pairs (my_list):
+    list_of_prods = []
+    s = len(my_list)
+    for i in range(s // 2):
+        list_of_prods.append(my_list[i] * my_list[s - i - 1])
+    if s % 2:
+        list_of_prods.append(my_list[s // 2])
+    return list_of_prods
+s = int(input("Введите длину списка: "))
+list_num = new_list(s)
+print(list_num)
+print(prod_of_pairs(list_num))
