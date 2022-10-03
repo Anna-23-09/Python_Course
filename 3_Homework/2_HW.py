c879bad19b1000327d60dcc886dee6598dd686c9
Anna <tolstovaaniuta@yandex.ru>
2022-10-03 13:24:38 +0300

    1 task

---
+++ 3_Homework/2_HW.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+from random import sample
+def prod (count, number):
+    my_list = sample(range(1, 11), count)
+    print(my_list)
+    result = 1
+    n = 1
+    if n in 
+    
+    
+print(num_in_list(int(input()), int(input())))
