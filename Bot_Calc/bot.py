34c9460dae5234f27d220518d2c3daaaf7ffc189
Anna <tolstovaaniuta@yandex.ru>
2022-11-09 11:37:45 +0300

    первые наметки

--- Bot_Calc/bot.py
+++ Bot_Calc/bot.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 # Определяем константы этапов разговора
-TYPE, ACTION = range(2)
+TYPE_N, ACTION = range(2)
 
 # функция обратного вызова точки входа в разговор
 def start(update, _):
@@ -25,10 +25,27 @@ def start(update, _):
         'Команда /cancel, чтобы прекратить разговор.\n\n'
         'С какими числами будем работать?',
         reply_markup=markup_key,)
-    # переходим к этапу `GENDER`, это значит, что ответ
+    # переходим к этапу `TYPE`, это значит, что ответ
     # отправленного сообщения в виде кнопок будет список 
-    # обработчиков, определенных в виде значения ключа `GENDER`
-    return TYPE
+    # обработчиков, определенных в виде значения ключа `TYPE`
+    return TYPE_N
+
+
+    # Обрабатываем пол пользователя
+def type_n(update, _):
+    # Список кнопок для ответа
+    reply_keyboard = [['ADD', 'SUB', 'MULTI', 'DIV', 'ROOT', 'SQRT']]
+    # определяем пользователя
+    user = update.message.from_user
+    # Пишем в журнал тип числа:
+    logger.info("Тип %s: %s", user.first_name, update.message.text)
+    # Следующее сообщение с удалением клавиатуры `ReplyKeyboardRemove`
+    update.message.reply_text(
+        'Хорошо. Что с ним будем делать?',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    # переходим к этапу `PHOTO`
+    return PHOTO
 
 
 
@@ -45,8 +62,8 @@ def start(update, _):
         entry_points=[CommandHandler('start', start)],
         # этапы разговора, каждый со своим списком обработчиков сообщений
         states={
-            TYPE: [MessageHandler(Filters.regex('^(RATIONAL|COMPLEX)$'), gender)],
-            ACTION: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
+            TYPE_N: [MessageHandler(Filters.regex('^(RATIONAL|COMPLEX)$'), type_n)],
+            ACTION: [MessageHandler(Filters.regex('^(PLUS|MINUS|)$'), action)],
             LOCATION: [
                 MessageHandler(Filters.location, location),
                 CommandHandler('skip', skip_location),
